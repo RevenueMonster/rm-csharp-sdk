f9f44b4ab875e560c63a143341e59fc657fdb0ec
Simon Lim <xin.jong@hotmail.com>
2019-02-01 11:49:29 +0800

    Add not binding environment variable endpoint

--- Authorization.cs
+++ Authorization.cs
@@ -59,6 +59,54 @@ namespace RevenueMonsterOpenAPI
             return result;
         }
 
+        public async Task<ClientCredentials> GetClientCredentials(string clientId, string clientSecret, string environment)
+        {
+            ClientCredentials result = new ClientCredentials();
+            try
+            {
+                string parameter = String.Concat(clientId, ":", clientSecret);
+                string encodeParameter = Encode.Base64Encode(parameter);
+                string url = "";
+                if (environment == "sandbox")
+                {
+                    url = String.Concat(Url.SandBoxOAuth, "/v1/token");
+                }
+                else if (environment == "production")
+                {
+                    url = String.Concat(Url.ProductionOAuth, "/v1/token");
+                }
+
+                var values = new
+                {
+                    grantType = "client_credentials"
+                };
+
+                var content = JsonConvert.SerializeObject(values);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodeParameter);
+                var response = await client.PostAsync(url, byteContent);
+                var responseStr = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonAsString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<ClientCredentials>(jsonAsString);
+                }
+                else
+                {
+                    result = JsonConvert.DeserializeObject<ClientCredentials>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error", ex.Message);
+            }
+            return result;
+        }
+
         public async Task<ClientCredentials> RefreshToken()
         {
             ClientCredentials result = new ClientCredentials();
@@ -70,7 +118,7 @@ namespace RevenueMonsterOpenAPI
                 if (ProjectEnvironment.environment == "sandbox")
                 {
                     url = String.Concat(Url.SandBoxOAuth, "/v1/token");
-                }
+                } 
                 else if (ProjectEnvironment.environment == "production")
                 {
                     url = String.Concat(Url.ProductionOAuth, "/v1/token");
@@ -80,6 +128,55 @@ namespace RevenueMonsterOpenAPI
                 {
                     grantType = "refresh_token",
                     refreshToken = ProjectEnvironment.refreshToken
+                };
+
+                var content = JsonConvert.SerializeObject(values);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodeParameter);
+                var response = await client.PostAsync(url, byteContent);
+                var responseStr = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonAsString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<ClientCredentials>(jsonAsString);
+                }
+                else
+                {
+                    result = JsonConvert.DeserializeObject<ClientCredentials>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error", ex.Message);
+            }
+            return result;
+        }
+
+        public async Task<ClientCredentials> RefreshToken(string clientId, string clientSecret, string refreshToken, string environment)
+        {
+            ClientCredentials result = new ClientCredentials();
+            try
+            {
+                string parameter = String.Concat(clientId, ":", clientSecret);
+                string encodeParameter = Encode.Base64Encode(parameter);
+                string url = "";
+                if (environment == "sandbox")
+                {
+                    url = String.Concat(Url.SandBoxOAuth, "/v1/token");
+                }
+                else if (environment == "production")
+                {
+                    url = String.Concat(Url.ProductionOAuth, "/v1/token");
+                }
+
+                var values = new
+                {
+                    grantType = "refresh_token",
+                    refreshToken = refreshToken
                 };
 
                 var content = JsonConvert.SerializeObject(values);
--- Voucher.cs
+++ Voucher.cs
@@ -65,6 +65,60 @@ namespace RevenueMonsterOpenAPI
             return result;
         }
 
+        public async Task<IssueVoucherResult> IssueVoucher(string batchKey, string privateKey, string accessToken, string environment)
+        {
+            Object data = new { };
+            string compactJson = SignatureUtil.GenerateCompactJson(data);
+            string encode = Encode.Base64Encode(compactJson);
+            string method = "post";
+            string nonceStr = RandomString.GenerateRandomString(32);
+            string requestUrl = "";
+            if (environment == "sandbox")
+            {
+                requestUrl = String.Concat(Url.SandBoxOpen, "/v3/voucher-batch/" + batchKey + "/issue");
+            }
+            else if (environment == "production")
+            {
+                requestUrl = String.Concat(Url.ProductionOpen, "/v3/voucher-batch/" + batchKey + "/issue");
+            }
+            string signType = "sha256";
+            string timestamp = Convert.ToString((Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+            Signature signature = new Signature();
+            string signatureResult = "";
+            signatureResult = signature.GenerateSignature(null, method, nonceStr, privateKey, requestUrl, signType, timestamp, environment);
+            signatureResult = "sha256 " + signatureResult;
+            IssueVoucherResult result = new IssueVoucherResult();
+            try
+            {
+                var content = JsonConvert.SerializeObject(data);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                client.DefaultRequestHeaders.Add("X-Nonce-Str", nonceStr);
+                client.DefaultRequestHeaders.Add("X-Signature", signatureResult);
+                client.DefaultRequestHeaders.Add("X-Timestamp", timestamp);
+                var response = await client.PostAsync(requestUrl, byteContent);
+                var responseStr = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonAsString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<IssueVoucherResult>(jsonAsString);
+                }
+                else
+                {
+                    result = JsonConvert.DeserializeObject<IssueVoucherResult>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error", ex.Message);
+            }
+            return result;
+        }
+
         public async Task<VoidVoucherResult> VoidVoucher(string code)
         {
             Object data = new { };
@@ -96,6 +150,60 @@ namespace RevenueMonsterOpenAPI
                 byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
                 HttpClient client = new HttpClient();
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ProjectEnvironment.accessToken);
+                client.DefaultRequestHeaders.Add("X-Nonce-Str", nonceStr);
+                client.DefaultRequestHeaders.Add("X-Signature", signatureResult);
+                client.DefaultRequestHeaders.Add("X-Timestamp", timestamp);
+                var response = await client.PostAsync(requestUrl, byteContent);
+                var responseStr = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonAsString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<VoidVoucherResult>(jsonAsString);
+                }
+                else
+                {
+                    result = JsonConvert.DeserializeObject<VoidVoucherResult>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error", ex.Message);
+            }
+            return result;
+        }
+
+        public async Task<VoidVoucherResult> VoidVoucher(string code, string privateKey, string accessToken, string environment)
+        {
+            Object data = new { };
+            string compactJson = SignatureUtil.GenerateCompactJson(data);
+            string encode = Encode.Base64Encode(compactJson);
+            string method = "post";
+            string nonceStr = RandomString.GenerateRandomString(32);
+            string requestUrl = "";
+            if (environment == "sandbox")
+            {
+                requestUrl = String.Concat(Url.SandBoxOpen, "/v3/voucher/" + code + "/void");
+            }
+            else if (environment == "production")
+            {
+                requestUrl = String.Concat(Url.ProductionOpen, "/v3/voucher/" + code + "/void");
+            }
+            string signType = "sha256";
+            string timestamp = Convert.ToString((Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+            Signature signature = new Signature();
+            string signatureResult = "";
+            signatureResult = signature.GenerateSignature(null, method, nonceStr, privateKey, requestUrl, signType, timestamp, environment);
+            signatureResult = "sha256 " + signatureResult;
+            VoidVoucherResult result = new VoidVoucherResult();
+            try
+            {
+                var content = JsonConvert.SerializeObject(data);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
                 client.DefaultRequestHeaders.Add("X-Nonce-Str", nonceStr);
                 client.DefaultRequestHeaders.Add("X-Signature", signatureResult);
                 client.DefaultRequestHeaders.Add("X-Timestamp", timestamp);
@@ -166,6 +274,53 @@ namespace RevenueMonsterOpenAPI
             return result;
         }
 
+        public async Task<GetVoucherByCodeResult> GetVoucherByCode(string code, string privateKey, string accessToken, string environment)
+        {
+            string method = "get";
+            string nonceStr = RandomString.GenerateRandomString(32);
+            string requestUrl = "";
+            if (environment == "sandbox")
+            {
+                requestUrl = String.Concat(Url.SandBoxOpen, "/v3/voucher/" + code);
+            }
+            else if (environment == "production")
+            {
+                requestUrl = String.Concat(Url.ProductionOpen, "/v3/voucher/" + code);
+            }
+            string signType = "sha256";
+            string timestamp = Convert.ToString((Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+            Signature signature = new Signature();
+            string signatureResult = "";
+            signatureResult = signature.GenerateSignature("", method, nonceStr, privateKey, requestUrl, signType, timestamp, environment);
+            signatureResult = "sha256 " + signatureResult;
+            GetVoucherByCodeResult result = new GetVoucherByCodeResult();
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                client.DefaultRequestHeaders.Add("X-Nonce-Str", nonceStr);
+                client.DefaultRequestHeaders.Add("X-Signature", signatureResult);
+                client.DefaultRequestHeaders.Add("X-Timestamp", timestamp);
+                var response = await client.GetAsync(requestUrl);
+                var responseStr = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonAsString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<GetVoucherByCodeResult>(jsonAsString);
+                }
+                else
+                {
+                    result = JsonConvert.DeserializeObject<GetVoucherByCodeResult>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error", ex.Message);
+            }
+            return result;
+        }
+
         public async Task<GetVoucherBatchesResult> GetVoucherBatches()
         {
             string method = "get";
@@ -190,6 +345,53 @@ namespace RevenueMonsterOpenAPI
             {
                 HttpClient client = new HttpClient();
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ProjectEnvironment.accessToken);
+                client.DefaultRequestHeaders.Add("X-Nonce-Str", nonceStr);
+                client.DefaultRequestHeaders.Add("X-Signature", signatureResult);
+                client.DefaultRequestHeaders.Add("X-Timestamp", timestamp);
+                var response = await client.GetAsync(requestUrl);
+                var responseStr = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonAsString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<GetVoucherBatchesResult>(jsonAsString);
+                }
+                else
+                {
+                    result = JsonConvert.DeserializeObject<GetVoucherBatchesResult>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error", ex.Message);
+            }
+            return result;
+        }
+
+        public async Task<GetVoucherBatchesResult> GetVoucherBatches(string privateKey, string accessToken, string environment)
+        {
+            string method = "get";
+            string nonceStr = RandomString.GenerateRandomString(32);
+            string requestUrl = "";
+            if (environment == "sandbox")
+            {
+                requestUrl = String.Concat(Url.SandBoxOpen, "/v3/voucher-batches");
+            }
+            else if (environment == "production")
+            {
+                requestUrl = String.Concat(Url.ProductionOpen, "/v3/voucher-batches");
+            }
+            string signType = "sha256";
+            string timestamp = Convert.ToString((Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+            Signature signature = new Signature();
+            string signatureResult = "";
+            signatureResult = signature.GenerateSignature("", method, nonceStr, privateKey, requestUrl, signType, timestamp, environment);
+            signatureResult = "sha256 " + signatureResult;
+            GetVoucherBatchesResult result = new GetVoucherBatchesResult();
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
                 client.DefaultRequestHeaders.Add("X-Nonce-Str", nonceStr);
                 client.DefaultRequestHeaders.Add("X-Signature", signatureResult);
                 client.DefaultRequestHeaders.Add("X-Timestamp", timestamp);
@@ -238,6 +440,53 @@ namespace RevenueMonsterOpenAPI
             {
                 HttpClient client = new HttpClient();
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ProjectEnvironment.accessToken);
+                client.DefaultRequestHeaders.Add("X-Nonce-Str", nonceStr);
+                client.DefaultRequestHeaders.Add("X-Signature", signatureResult);
+                client.DefaultRequestHeaders.Add("X-Timestamp", timestamp);
+                var response = await client.GetAsync(requestUrl);
+                var responseStr = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonAsString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<GetVoucherBatchByKeyResult>(jsonAsString);
+                }
+                else
+                {
+                    result = JsonConvert.DeserializeObject<GetVoucherBatchByKeyResult>(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error", ex.Message);
+            }
+            return result;
+        }
+
+        public async Task<GetVoucherBatchByKeyResult> GetVoucherBatchByKey(string batchKey, string privateKey, string accessToken, string environment)
+        {
+            string method = "get";
+            string nonceStr = RandomString.GenerateRandomString(32);
+            string requestUrl = "";
+            if (environment == "sandbox")
+            {
+                requestUrl = String.Concat(Url.SandBoxOpen, "/v3/voucher-batch/" + batchKey);
+            }
+            else if (environment == "production")
+            {
+                requestUrl = String.Concat(Url.ProductionOpen, "/v3/voucher-batch/" + batchKey);
+            }
+            string signType = "sha256";
+            string timestamp = Convert.ToString((Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+            Signature signature = new Signature();
+            string signatureResult = "";
+            signatureResult = signature.GenerateSignature("", method, nonceStr, privateKey, requestUrl, signType, timestamp, environment);
+            signatureResult = "sha256 " + signatureResult;
+            GetVoucherBatchByKeyResult result = new GetVoucherBatchByKeyResult();
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
                 client.DefaultRequestHeaders.Add("X-Nonce-Str", nonceStr);
                 client.DefaultRequestHeaders.Add("X-Signature", signatureResult);
                 client.DefaultRequestHeaders.Add("X-Timestamp", timestamp);
